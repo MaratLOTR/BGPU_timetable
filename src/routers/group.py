872626743e20f720285df81de47105b487814384
MaratLOTR <maratabdrahmanov@yandex.ru>
2023-05-06 12:52:45 +0500

    Переписано использование функций получения даты и дня недели, теперь используются функции из utils.date

--- src/routers/group.py
+++ src/routers/group.py
@@ -3,42 +3,7 @@ router = APIRouter()
 from db_pack import fake_query
 import datetime
 from .create_new_schedule import create_fake_schedule
-def get_day_of_the_week_by_date(date):
-    day_of_the_week = {1:"Пн",
-                       2:"Вт",
-                       3:"Ср",
-                       4:"Чт",
-                       5:"Пт",
-                       6:"Сб",
-                       7:"Вс"}
-    number_day_of_the_week = date.isoweekday()
-    return day_of_the_week[number_day_of_the_week]
-
-def get_days_by_week(number_of_week:int):
-    date = datetime.datetime.strptime('2022-09-05', "%Y-%m-%d")
-    date = date + datetime.timedelta(weeks=int(number_of_week))
-    date_day_of_week = []
-
-    for i in range(6):
-        day_of_week = get_day_of_the_week_by_date(date)
-        date_day_of_week.append({'date': date.day, "day_of_the_week": day_of_week})
-        date = date + datetime.timedelta(days=1)
-    return date_day_of_week
-
-
-@router.get("/group/{group}")
-def group_filter(faculty: str, course: str, group: str, week: int):
-    return fake_query.get_group_schedule(faculty=faculty, course=course,week=week, group=group)
-
-@router.get("/group")
-def get_enable_filter_group():
-    return fake_query.get_list_enable_value_group()
-
-@router.get("/auditory")
-def get_enable_filter_auditory():
-    return
-# @router.get("/schedule_123")
-
+from utils.date import get_days_by_week
 @router.get("/getDaysOfWeek")
 def get_date_by_week(week:str):
     return [get_days_by_week(int(week))]
@@ -55,9 +20,5 @@ def get_schedule_for_day(group: str, week: str, day_of_the_week: str):
             del response_sch['groupName']
             del response_sch['date']
             del response_sch['dayOfTheWeek']
-            # response_sch.pop('week')
-            # response_sch.pop('groupName')
-            # response_sch.pop('date')
-            # response_sch.pop('dayOfTheWeek')
             schedule_in_a_day.append(response_sch)
     return schedule_in_a_day
