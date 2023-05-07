6838386bb965574dea62ead01af573b2fcb587fd
MaratLOTR <maratabdrahmanov@yandex.ru>
2023-05-07 18:39:06 +0500

    Добавлены Endopoint для получения фильтров факультета и кафедры

--- src/routers/teacher_filter.py
+++ src/routers/teacher_filter.py
@@ -3,16 +3,19 @@ from utils.TestData import get_all_faculty, get_cafedra_by_faculty
 
 router = APIRouter()
 
-@router.get("/getFaculty")
+@router.get("/getFilterFacultyTeacher")
 def get_filter_teacher():
         return get_all_faculty()
 
-@router.get("/getCafedraByFaculty")
+@router.get("/getFiltersCafedraWeek")
 def get_filter_teacher(faculty: str):
-        return get_cafedra_by_faculty(faculty)
+        return [{'cafedra':get_cafedra_by_faculty(faculty),
+                 'week': [number_week for number_week in range(1,10)]}]
 
 
-
+@router.get("/getTeacherByFilter")
+def get_teacher_by_filter(faculty: str, cafedra: str):
+        get
 
 # @router.get("/groupByFilter")
 # def group_by_filter(faculty: str, course: int):
