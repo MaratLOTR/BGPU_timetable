a9fbb4ad116b37dc87a35e1c1ffbcf009a3b62b8
MaratLOTR <maratabdrahmanov@yandex.ru>
2023-05-08 21:24:04 +0500

    Изменен Endpoint "filter_faculty" на тот, что был у Семена

--- src/routers/filter_group.py
+++ src/routers/filter_group.py
@@ -9,22 +9,14 @@ router = APIRouter()
 #ПРАВИЛЬНАЯ ВЕРСИЯ У ТЕБЯ, ПОЭТОМУ ОСТАВЛЯЙ СВОЙ ВАРИАНТ
 #!!!
 #Начало спорного кода
-@router.get("/filter_faculty")
+@router.get("/getFilterFacultyCourseWeek")
 def get_all_faculty():
-    return  [
-                "ФИРТ",
-                "ИНЭК",
-                ]
-
-@router.get("/filter_course")
-def get_all_course():
-    return list(i for i in range(1,5))
-
-@router.get("/filter_week")
-def get_all_week():
-    return  list(i for i in range(1,25))
+    return [{"faculty": ["ФИРТ", "ИНЭК", ], "course": list(str(i) for i in range(1, 5)),
+             "week": list(str(i) for i in range(1, 25))}
+            ]
 
 ###КОНЕЦ СПОРНОГО КОДА
 @router.get("/groupByFilter")
 def group_by_filter(faculty: str, course: int):
     return get_group_by_filter(faculty=faculty, course=course)
+
